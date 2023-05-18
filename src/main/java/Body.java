e9d79e5cded8508d9f21ffc9eee25489f634aba1
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-05-18 13:59:50 -0400

    adding D&D aspects :warrior class

---
+++ src/main/java/Body.java
@@ -0,0 +1,17 @@
+public class Body {
+    public String rArm;
+    public String lArm;
+    public String rLeg;
+    public String lLeg;
+    public String head;
+    public String torso;
+
+    public Body(String rArm, String lArm, String rLeg, String lLeg, String head, String torso) {
+        this.rArm = rArm;
+        this.lArm = lArm;
+        this.rLeg = rLeg;
+        this.lLeg = lLeg;
+        this.head = head;
+        this.torso = torso;
+    }
+}
---
+++ src/main/java/Warrior.java
@@ -0,0 +1,11 @@
+public class Warrior extends Body{
+   public int strength=10;
+   public int awarness=5;
+   public int dexterty=5;
+
+
+    public Warrior(String rArm, String lArm, String rLeg, String lLeg, String head, String torso) {
+        super(rArm, lArm, rLeg, lLeg, head, torso);
+    }
+}
+
