bfbbe6ad21a906046f4d455d1b628ed897072007
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-05-30 15:56:42 -0400

    populates grid with 1's

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Map {
     public static int[][] matrix = new int[4][4];
-    public String[] rows = new String[4];
+
     public int upDown = 3;
     public int leftRight = 2;
     public Scanner scanner = new Scanner(System.in);
@@ -15,26 +15,21 @@ public class Map {
     public boolean validPathWest = false;
 
     public Map() {
-        rows[0] = "00100";
-        rows[1] = "00100";
-        rows[2] = "01000";
-        rows[3] = "00400";
+
         System.out.println("Map starts at "+matrix[0][2]);
 
 
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix.length; col++) {
-
-
+                   matrix[col][row]=1;
             }
         }
+        matrix[3][2]=0;
     }
 
     public void showMap() {
         System.out.println("MAP");
-        for (String row : rows) {
-            System.out.println(row);
-        }
+
 
     }
 
